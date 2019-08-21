903c07dd1374562d5243333cf8bd7a349c37b9fc
Abraao barros lacerda <abraaobarros3@gmail.com>
2019-08-21 17:53:02 -0300

    Add ql score

--- query/index.js
+++ query/index.js
@@ -14,6 +14,7 @@ const handlers = {
     legendas: getParties,
     candidatos: getCandidates,
     votos_mun: getMunVotes,
+    ql: getQl,
 };
 
 exports.handler = async function(event, context) {
@@ -61,7 +62,7 @@ function getCandidates(params) {
 }
 
 function getMunVotes(params) {
-    let { uf, year, turn, candidate_id } = params;
+    let { uf, year, turn, candidate_id,  } = params;
     
     return db('votos_mun')
         .select([
@@ -85,6 +86,30 @@ function getMunVotes(params) {
         .where('ano_eleicao', year)
         .where('num_turno', turn)
         .where('id_candidato', candidate_id);
+}
+
+function getQl( params ){
+    let { uf, year, turn, candidate_id, position } = params;
+    db.raw(`
+    select numero_candidato, 
+    cod_mun_tse, 
+    total_votos_estado, 
+    total_votos_cand, 
+    qtde_votos_mun, 
+    (qtde_votos * total_votos_estado) / (total_votos_cand * qtde_votos_mun) as QL 
+    from(
+        select numero_candidato, 
+        m.cod_mun_tse, m.qtde_votos, 
+        s.qtde_votos as qtde_votos_mun,
+        (select sum(qtde_votos) 
+            from votos_mun where ano_eleicao = '${year}' and uf = '${uf.toUpperCase()}' and codigo_cargo = '${position}' and id_candidato = ${candidate_id}) as total_votos_cand,
+        (select sum(qtde_votos) 
+            from votos_mun where ano_eleicao = '${year}' and uf = '${uf.toUpperCase()}' and codigo_cargo = '${position}') as total_votos_estado
+    from votos_mun as m
+    join(select cod_mun_tse, 
+        sum(qtde_votos) as qtde_votos 
+        from votos_mun where ano_eleicao = '${year}' and uf = '${uf.toUpperCase()}' and codigo_cargo = '${position}' group by cod_mun_tse) as s on m.cod_mun_tse = s.cod_mun_tse
+    where id_candidato = ${candidate_id}) as t`)
 }
 
 function createResponse(result, statusCode) {
