76889f24652f477b0c9eb28371c612d9e35f657d
EC2 Default User <ec2-user@ip-172-31-91-74.ec2.internal>
2019-08-22 13:59:37 +0000

    Fixed query

--- query/index.js
+++ query/index.js
@@ -17,27 +17,28 @@ const handlers = {
     ql: getQl,
 };
 
-exports.handler = async function(event, context) {
-    context.callbackWaitsForEmptyEventLoop = false; 
+exports.handler = async function (event, context) {
+    context.callbackWaitsForEmptyEventLoop = false;
     const { table, ...params } = event.queryStringParameters || {};
-    
+
     try {
         let action = handlers[table];
         if (action)
             return createResponse(await action(params));
         else
             throw Error("Invalid Table");
-    } catch (e) {
+    }
+    catch (e) {
         return createResponse("Could not run query: " + e, 402);
     }
-       
+
 };
 
 function getParties(params) {
     let { uf, position, year, turn } = params;
-    
+
     if (position == 1) uf = "BR";
-    
+
     return db('candidatos')
         .select(['sigla_partido', 'numero_partido'])
         .where('sigla_uf', uf.toUpperCase())
@@ -49,9 +50,9 @@ function getParties(params) {
 
 function getCandidates(params) {
     let { uf, position, year, turn, party } = params;
-    
+
     if (position == 1) uf = "BR";
-    
+
     return db('candidatos')
         .select(['id_candidato', 'nome_candidato', 'numero_candidato'])
         .where('sigla_uf', uf.toUpperCase())
@@ -63,7 +64,7 @@ function getCandidates(params) {
 
 function getMunVotes(params) {
     let { uf, year, turn, candidate_id } = params;
-    
+
     return db('votos_mun')
         .select([
             'id_candidato',
@@ -90,8 +91,7 @@ function getMunVotes(params) {
 
 function getQl(params) {
     let { uf, year, turn, candidate_id, position } = params;
-    
-    return  db.raw(`
+    const response = db.raw(`
         select numero_candidato, 
         cod_mun_tse, 
         total_votos_estado, 
@@ -103,15 +103,17 @@ function getQl(params) {
             m.cod_mun_tse, m.qtde_votos, 
             s.qtde_votos as qtde_votos_mun,
             (select sum(qtde_votos) 
-                from votos_mun where ano_eleicao = '${year}' and uf = '${uf.toUpperCase()}' and turn = ${turn} and codigo_cargo = '${position}' and id_candidato = ${candidate_id}) as total_votos_cand,
+                from votos_mun where ano_eleicao = '${year}' and uf = '${uf.toUpperCase()}' and num_turno = ${turn} and codigo_cargo = ${position} and id_candidato = ${candidate_id}) as total_votos_cand,
             (select sum(qtde_votos) 
-                from votos_mun where ano_eleicao = '${year}' and uf = '${uf.toUpperCase()}' and turn = ${turn} and codigo_cargo = '${position}') as total_votos_estado
+                from votos_mun where ano_eleicao = '${year}' and uf = '${uf.toUpperCase()}' and num_turno = ${turn} and codigo_cargo = ${position}) as total_votos_estado
         from votos_mun as m
         join(select cod_mun_tse, 
             sum(qtde_votos) as qtde_votos 
-            from votos_mun where ano_eleicao = '${year}' and uf = '${uf.toUpperCase()}' and turn = ${turn} and codigo_cargo = '${position}' group by cod_mun_tse) as s on m.cod_mun_tse = s.cod_mun_tse
+            from votos_mun where ano_eleicao = '${year}' and uf = '${uf.toUpperCase()}' and num_turno = ${turn} and codigo_cargo = ${position} group by cod_mun_tse) as s on m.cod_mun_tse = s.cod_mun_tse
         where id_candidato = ${candidate_id}) as t
     `);
+
+    return response.rows;
 }
 
 function createResponse(result, statusCode) {
@@ -119,9 +121,9 @@ function createResponse(result, statusCode) {
         statusCode,
         body: JSON.stringify(result),
         headers: {
-          "Access-Control-Allow-Credentials": true,
-          "Access-Control-Allow-Origin": "*",
-          "Content-Type": "application/json",
+            "Access-Control-Allow-Credentials": true,
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json",
         }
     };
 }
